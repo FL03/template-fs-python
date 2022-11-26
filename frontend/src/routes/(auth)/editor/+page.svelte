8a4aa9ef17626b399f4ab92553689e68b8414c02
Joe McCain III <jo3mccain@scattered-systems.com>
2022-11-26 19:37:56 +0000

    Update

--- frontend/src/routes/(auth)/editor/+page.svelte
+++ frontend/src/routes/(auth)/editor/+page.svelte
@@ -1,24 +1,14 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
-    import { Editor, Viewer } from 'bytemd'
-    import gfm from '@bytemd/plugin-gfm'
-    import 'bytemd/dist/index.css';
-    
-    let value = '';
-    let plugins = [gfm()]
+  /** @type {import('./$types').PageData} */
+  export let data;
+  import { Editor, Viewer } from 'bytemd'
+  import gfm from '@bytemd/plugin-gfm'
+  import 'bytemd/dist/index.css';
   
-    function handleChange(e) {
-      value = e.detail.value
-    }
-  </script>
-  
-  <div class="flex flex-auto items-center justify-between py-24 min-h-full max-h-screen min-w-full max-w-screen">
-    <Editor
-        {value}
-        on:change={(e) => {
-        value = e.detail.value
-        }}
-        {plugins}
-    />
-  </div>
+  let plugins = [gfm()]
+
+  let value = data.content;
+</script>
+
+
+<Editor {plugins} {value} on:change={(e) => {value = e.detail.value}}/>
