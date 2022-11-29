b4691dc0168745f6959c14669508d1e498ad1fe5
Joe McCain III <jo3mccain@scattered-systems.com>
2022-11-29 05:33:02 +0000

    Update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
-    import abstractShapes from '$lib/assets/media/abstractShapes.svg';
+    function handleCredentialResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+    }
+		
+		
+    async function handleClick () {
+        google.accounts.id.initialize({
+            client_id: process.env.CLIENT_ID,
+            callback: handleCredentialResponse
+        });
+        google.accounts.id.renderButton(
+        document.getElementById("loginBtn"),
+        { theme: "outline", size: "large" }  // customization attributes
+        );
+        google.accounts.id.prompt(); // also display the One Tap dialog
+    }
 </script>
 
 <svelte:head>
@@ -11,8 +26,18 @@
 
 
 <div class="flex flex-auto items-center justify-center rounded">
-    <div class="rounded flex"><img alt="#" src="{abstractShapes}" class="flex "/></div>
     <span class="prose text-center text-white">
         Welcome
     </span>
 </div>
+
+<form class="flex flex-auto flex-col min-h-full rounded-lg">
+    <input autocomplete=""/>
+     <!-- <button
+        class="overflow-x-hidden py-1 px-2 inline-block mx-auto px-3 py-1 rounded items-center justify-center bg-gradient-to-r from-cyan-700 via-cyan-500 to-cyan-900 text-white max-w-24"
+        id="loginBtn"
+        on:click={handleClick}
+        >
+        Test
+    </button> -->
+</form>
