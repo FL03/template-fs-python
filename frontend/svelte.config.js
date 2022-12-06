edf088756921f4182e2fcb12152c30c96158dd52
Joe McCain III <jo3mccain@icloud.com>
2022-12-06 21:13:21 +0000

    Update

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,19 @@
 // import adapter from '@sveltejs/adapter-auto';
+
 import adapter from '@sveltejs/adapter-node';
+import adapter_vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
+
 
 
 const config = {
 	kit: {
-		adapter: adapter()
+		...(process.env.MODE === "staging") && {
+			adapter: adapter_vercel()
+		},
+		...(process.env.MODE !== "staging") && {
+			adapter: adapter()
+		}
 	},
 	preprocess: [
 		preprocess({
